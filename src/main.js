434a5430afecce8626b8f869c0a5a4cc3a2646b6
Burak Can Kocak <burak.c.kocak@gmail.com>
2020-05-10 11:23:31 +0200

    repo created.

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+//Bootstrap
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <MainView username="Burak Kocak" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainView from "@/components/MainView.vue";
+
+export default {
+  name: "mainview",
+  components: {
+    MainView,
+  },
+};
+</script>
