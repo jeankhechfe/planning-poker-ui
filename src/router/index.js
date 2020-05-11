3f3d6e850c2aa15587077b4aa6d5627973fad2dc
jean khechfe <jeankhechfe@gmail.com>
2020-05-11 14:19:24 +0200

    create about page

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,25 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Sample from "../views/Sample.vue";
 import Main from "../views/Main.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/sample",
-    name: "Sample",
-    component: Sample,
-  },
-  {
     path: "/",
     name: "Main",
     component: Main,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/sample",
+    name: "Sample",
+    component: Sample,
   },
 ];
 
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about container">
+    <div class="text-center">
+      <img alt="Vue logo" src="../assets/poker-logo.png" />
+      <h1>The Best Agile Planning Tool</h1>
+    </div>
+    <p>Planning Poker is a digital card game designed to help agile and scrum development teams effectively set their sprint goals through collaborative planning and consensus-based estimations. Planning Poker is proven to be one of the most effective sprint planning tools for agile teams.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
