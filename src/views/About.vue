badd7dd6c5ec57763bb28adeef0f56c8ff76f1e4
jean khechfe <jeankhechfe@gmail.com>
2020-05-11 21:48:56 +0200

    create about component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="about">
       <Header />
-    <div class="text-center">
-      <img alt="Vue logo" src="../assets/poker-logo.png" />
-      <h1>The Best Agile Planning Tool</h1>
-    </div>
-    <p>Planning Poker is a digital card game designed to help agile and scrum development teams effectively set their sprint goals through collaborative planning and consensus-based estimations. Planning Poker is proven to be one of the most effective sprint planning tools for agile teams.</p>
+      <AboutContent />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import AboutContent from "@/components/AboutContent.vue";
 
 export default {
   name: "About",
     components: {
     Header,
+    AboutContent,
   },
 };
 </script>
