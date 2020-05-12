2312d86c2a5eeb11fe2fff7b87f6e95bdab035db
Burak Can Kocak <burak.c.kocak@gmail.com>
2020-05-12 03:20:54 +0200

    Removed sample component and view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Sample from "../views/Sample.vue";
 import Main from "../views/Main.vue";
 import About from "../views/About.vue";
 import Login from "../views/Login.vue";
@@ -28,11 +27,6 @@ const routes = [
     path: "/register",
     name: "Register",
     component: Register,
-  },
-  {
-    path: "/sample",
-    name: "Sample",
-    component: Sample,
   },
 ];
 
