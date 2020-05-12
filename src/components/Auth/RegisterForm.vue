b38733fb54df4d133348428e2591d1bd3cf0edbe
jean khechfe <jeankhechfe@gmail.com>
2020-05-12 11:19:08 +0200

    username to text type

--- src/components/Auth/RegisterForm.vue
+++ src/components/Auth/RegisterForm.vue
@@ -14,7 +14,7 @@
                             <div class="form-group row">
                                 <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" required autofocus>
+                                    <input id="username" type="text" class="form-control" name="username" required autofocus>
                                 </div>
                             </div>
                             <div class="form-group row">
