c9a56ffd5b7b037367dc6db4140955e4c8098b68
Burak Can Kocak <burak.c.kocak@gmail.com>
2020-05-12 03:21:35 +0200

    paragraph containerized for better look

--- src/components/AboutContent.vue
+++ src/components/AboutContent.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        <div class="text-center">
-            <img alt="Vue logo" src="../assets/poker-logo.png" />
-            <h1>The Best Agile Planning Tool</h1>
-        </div>
-        <p>Planning Poker is a digital card game designed to help agile and scrum development teams effectively set their sprint goals through collaborative planning and consensus-based estimations. Planning Poker is proven to be one of the most effective sprint planning tools for agile teams.</p>
+  <div>
+    <div class="text-center">
+      <img alt="Vue logo" src="../assets/poker-logo.png" />
+      <h1>The Best Agile Planning Tool</h1>
     </div>
+    <div class="container">
+      <p style="margin: 30px">
+        Planning Poker is a digital card game designed to help agile and scrum
+        development teams effectively set their sprint goals through
+        collaborative planning and consensus-based estimations. Planning Poker
+        is proven to be one of the most effective sprint planning tools for
+        agile teams.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
