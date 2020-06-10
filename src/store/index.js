7e5352b0237bed2627f907bc5d0d3165887f42bf
Burak Can Kocak <burak.c.kocak@gmail.com>
2020-06-10 03:57:53 +0200

    persistent vuex configuration impl.

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import user from "./modules/user.js"
-import project from "./modules/project.js"
-import task from "./modules/task.js"
-import estimation from "./modules/estimation.js"
+import user from "./modules/user.js";
+import project from "./modules/project.js";
+import task from "./modules/task.js";
+import estimation from "./modules/estimation.js";
+import VuexPersist from "vuex-persist";
+
+const vuexPersist = new VuexPersist({
+  key: "pp-app",
+  storage: window.localStorage,
+});
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   modules: {
     user,
     project,
     task,
-    estimation
-  }
+    estimation,
+  },
 });
