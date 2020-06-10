bc7b493aa06a7116767c258664804c7ea8c47cd8
Burak Can Kocak <burak.c.kocak@gmail.com>
2020-06-10 05:03:31 +0200

    User is not allowed to change own vote when voting is finalized

--- src/components/VotingCards.vue
+++ src/components/VotingCards.vue
@@ -2,18 +2,42 @@
   <div class="container">
     <h2>Pick a Vote</h2>
     <div class="btn-group">
-      <button type="button" class="vote-btn" @click="estimate(1)">1 points</button>
-      <button type="button" class="vote-btn" @click="estimate(2)">2 points</button>
-      <button type="button" class="vote-btn" @click="estimate(3)">3 points</button>
-      <button type="button" class="vote-btn" @click="estimate(5)">5 points</button>
-      <button type="button" class="vote-btn" @click="estimate(8)">8 points</button>
-      <button type="button" class="vote-btn" @click="estimate(13)">13 points</button>
-      <button type="button" class="vote-btn" @click="estimate(21)">21 points</button>
-      <button type="button" class="vote-btn" @click="estimate(34)">34 points</button>
-      <button type="button" class="vote-btn" @click="estimate(55)">55 points</button>
-      <button type="button" class="vote-btn" @click="estimate(89)">89 points</button>
-      <button type="button" class="vote-btn" @click="estimate(99999)">Infinity</button>
-      <button type="button" class="vote-btn" @click="estimate(123456)">?</button>
+      <button type="button" class="vote-btn" @click="estimate(1)">
+        1 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(2)">
+        2 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(3)">
+        3 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(5)">
+        5 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(8)">
+        8 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(13)">
+        13 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(21)">
+        21 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(34)">
+        34 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(55)">
+        55 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(89)">
+        89 points
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(99999)">
+        Infinity
+      </button>
+      <button type="button" class="vote-btn" @click="estimate(123456)">
+        ?
+      </button>
     </div>
     <div v-show="isVoted" style="margin-top:30px">
       <h2>You voted : {{ user_estimation }}</h2>
@@ -29,17 +53,20 @@ export default {
   data() {
     return {
       isVoted: false,
-      user_estimation: 0
+      user_estimation: 0,
+      isEstimationFinalized: false,
     };
   },
 
   created() {
     axios
       .get("/tasks/" + this.$route.params.id + "/estimations")
-      .then(response => {
+      .then((response) => {
         if (response.status == 200) {
           this.estimations = response.data;
-          response.data.forEach(obj => {
+          response.data.forEach((obj) => {
+            this.isEstimationFinalized =
+              obj.task.estimation != 0 ? true : false;
             if (this.$store.getters.user.username == obj.user.login) {
               this.isVoted = true;
               if (obj.estimation == 99999) {
@@ -54,38 +81,40 @@ export default {
           this.$emit("set-Voted", this.isVoted);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   },
   methods: {
     estimate(estimation) {
-      axios
-        .post("/tasks/" + this.$route.params.id + "/estimations", {
-          estimation: estimation,
-          userId: this.$store.getters.user.token,
-          taskId: this.$route.params.id
-        })
-        .then(response => {
-          if (response.status == 200) {
-            this.isVoted = true;
-            this.$emit("set-Voted", this.isVoted);
-            if (estimation == 99999) {
-              this.user_estimation = "INFINITY";
-            } else if (estimation == 123456) {
-              this.user_estimation = "?";
-            } else {
-              this.user_estimation = estimation;
+      if (!this.isEstimationFinalized) {
+        axios
+          .post("/tasks/" + this.$route.params.id + "/estimations", {
+            estimation: estimation,
+            userId: this.$store.getters.user.token,
+            taskId: this.$route.params.id,
+          })
+          .then((response) => {
+            if (response.status == 200) {
+              this.isVoted = true;
+              this.$emit("set-Voted", this.isVoted);
+              if (estimation == 99999) {
+                this.user_estimation = "INFINITY";
+              } else if (estimation == 123456) {
+                this.user_estimation = "?";
+              } else {
+                this.user_estimation = estimation;
+              }
+              response.data.estimation = this.user_estimation;
+              this.$emit("add-estimation", response.data);
             }
-            response.data.estimation = this.user_estimation;
-            this.$emit("add-estimation", response.data);
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+  },
 };
 </script>
 
